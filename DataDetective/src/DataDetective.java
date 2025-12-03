859b49bbf60aa8e371ed998307766680c47e045c
SmortBlerb <SmortBlerb@gmail.com>
2025-12-02 22:58:22 -0700

    Fully finished data detective analysis

--- DataDetective/src/DataDetective.java
+++ DataDetective/src/DataDetective.java
@@ -1,10 +1,28 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class DataDetective {
     public static void main(String[] args) throws Exception {
-        int[] e = inputToList("88, 92, 75, 92, 100");
-        AnalyzeData(e);
-        System.out.println("done");
+        Scanner input = new Scanner(System.in);
+        String in = input.nextLine();
+        char[] inCharArray = in.toCharArray();
+
+        while(!checkInput(inCharArray)) {
+            System.out.println("WRONG");
+                
+            in = input.nextLine();
+            inCharArray = in.toCharArray();
+        }
+    }
+
+    public static boolean checkInput(char[] input) {
+        for(int i = 0; i < input.length; i++) {
+            if (!(Character.isDigit(input[i]) || input[i] == ',' || input[i] == ' ')) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static int[] inputToList(String input) {
@@ -24,8 +42,11 @@ public class DataDetective {
     }
 
     public static void AnalyzeData(int[] data) {
+        int[] sortedData = data;
+        Arrays.sort(sortedData);
+        
         // Actual data we want
-        int dataSize = data.length;
+        int dataSize = sortedData.length;
         double mean;
         int median = 0;
         int mode = 0;
@@ -36,40 +57,50 @@ public class DataDetective {
         double total = 0;
         int low = Integer.MAX_VALUE;
         int high = Integer.MIN_VALUE;
-        double trueMiddle;
-        double medianDiff = Integer.MAX_VALUE;
         ArrayList<Double> squaredDiffs = new ArrayList<>();
         double squaredDiffSum = 0;
-        double avgSquaredDiffs; 
-        String dataString = data.toString();
+        int modeAppearences = 0;
+        int prevModeAppearences = 0;
 
-        for(int i = 0; i < data.length; i++) { // Loop through the list of ints to get a bunch of sub-data
-            total += data[i]; // Sum total of data values
+        for(int i = 0; i < sortedData.length; i++) { // Loop through the list of ints to get a bunch of sub-data
+            total += sortedData[i]; // Sum total of data values
 
-            if(data[i] < low) { // Lowest and highest values
+            if(sortedData[i] < low) { // Lowest and highest values
                 low = data[i];
             }
-            if(data[i] > high) {
+            if(sortedData[i] > high) {
                 high = data[i];
             }
         }
         mean = (double)(total / dataSize); // Mean found!
         range = high - low;
-        trueMiddle = (double)((high + low) / 2);
-        System.out.println(low);
 
-        for(int i = 0; i < data.length; i++) { // Second pass with more data
-            if (Math.abs(trueMiddle - data[i]) < medianDiff) {
-                median = data[i];
-                medianDiff = Math.abs(trueMiddle - data[i]);
+        if(sortedData.length % 2 != 0) {
+            median = sortedData[(int)((sortedData.length / 2) + 0.5)];
+        } else {
+            median = sortedData[sortedData.length / 2] + (sortedData.length / 2) + 1 / 2;
+        }
+
+        for(int i = 0; i < sortedData.length - 1; i++) {
+            if(sortedData[i] == sortedData[i + 1]) {
+                modeAppearences++;
+            } else {
+                modeAppearences = 0;
             }
-            squaredDiffs.add(Math.sqrt((double)(mean - data[i])));
+
+            if(modeAppearences > prevModeAppearences) {
+                mode = sortedData[i];
+            }
+        }
+
+        for(int i = 0; i < sortedData.length; i++) { // Getting the squared difference for below
+            squaredDiffs.add(Math.pow(sortedData[i] - mean, 2));
         }
         
-        for(int i = 0; i < squaredDiffs.size(); i++) { // Third pass for standard deviation
+        for(int i = 0; i < squaredDiffs.size(); i++) { // Standard deviation
             squaredDiffSum += squaredDiffs.get(i);
         }
-        standardDeviation = Math.sqrt((double)(squaredDiffSum / squaredDiffs.size()));
+        standardDeviation = Math.sqrt((double)squaredDiffSum / (double)squaredDiffs.size());
 
         System.out.println("Mean = " + mean);
         System.out.println("Median = " + median);
